69a3c7277cffbb46d7dfe3fd45782ab2cb44b769
coru89 <corey.james.keller@gmail.com>
2024-09-14 14:56:06 +0700

    trying to fix tina

--- .tina/config.ts
+++ .tina/config.ts
@@ -48,8 +48,8 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
-      publicFolder: "media",
+      mediaRoot: "images",
+      publicFolder: "src",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
