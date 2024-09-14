46ef02fee3526c10ce9902765c6c8aa6f75ee441
coru89 <corey.james.keller@gmail.com>
2024-09-14 12:54:30 +0700

    build..

--- .tina/config.ts
+++ .tina/config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   media: {
     tina: {
       mediaRoot: "",
-      publicFolder: "dist",
+      publicFolder: "src",
     },
   },
   schema: {
--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -786,8 +786,8 @@ var config_default = defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "images",
-      publicFolder: "dist"
+      mediaRoot: "",
+      publicFolder: "src"
     }
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
