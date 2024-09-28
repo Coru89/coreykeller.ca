a5b2cbcdf4b8419574ad14ffe72f330708d1dfe4
coru89 <corey.james.keller@gmail.com>
2024-09-28 16:11:13 +0700

    removing hero animation

--- src/js/hero-animation.js
+++ src/js/hero-animation.js
@@ -1,32 +1,32 @@
-const header = document.querySelector("#header");
-const cursor = document.querySelector("#cursor");
-const texts = ["Hello, my name is"];
+// const header = document.querySelector("#header");
+// const cursor = document.querySelector("#cursor");
+// const texts = ["Hello, my name is"];
 
-let index = 0;
-let i = 0;
-let timeout;
+// let index = 0;
+// let i = 0;
+// let timeout;
 
-const writeText = () => {
-  if (i < texts[index].length) {
-    header.textContent += texts[index].charAt(i);
-    i++;
-    timeout = setTimeout(writeText, 50);
-  } else {
-    timeout = setTimeout(backspace, 2000);
-  }
-};
+// const writeText = () => {
+//   if (i < texts[index].length) {
+//     header.textContent += texts[index].charAt(i);
+//     i++;
+//     timeout = setTimeout(writeText, 50);
+//   } else {
+//     timeout = setTimeout(backspace, 2000);
+//   }
+// };
 
-const backspace = () => {
-  if (header.textContent.length > 0) {
-    header.textContent = header.textContent.slice(0, -1);
-    timeout = setTimeout(backspace, 50);
-  } else {
-    index = (index + 1) % texts.length;
-    header.textContent = "";
-    i = 0;
-    timeout = setTimeout(writeText, 1000);
-  }
-};
+// const backspace = () => {
+//   if (header.textContent.length > 0) {
+//     header.textContent = header.textContent.slice(0, -1);
+//     timeout = setTimeout(backspace, 50);
+//   } else {
+//     index = (index + 1) % texts.length;
+//     header.textContent = "";
+//     i = 0;
+//     timeout = setTimeout(writeText, 1000);
+//   }
+// };
 
-// Delay the start of the animation by 1 second (1000 milliseconds)
-setTimeout(backspace, 1200);
+// // Delay the start of the animation by 1 second (1000 milliseconds)
+// setTimeout(backspace, 1200);
